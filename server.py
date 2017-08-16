a42d001c2490fdda6d89bb49e1861fb3840a1b02
Arseny Kirichenko <mr.ars156@gmail.com>
2017-08-16 16:58:03 +0300

    Рефакторинг metrics

--- server.py
+++ server.py
@@ -1,17 +1,16 @@
 #! /usr/bin/env python
 import argparse
-import yaml
 from aiohttp import web, ClientSession, TCPConnector
 import async_timeout
 
 parser = argparse.ArgumentParser(description='Prometheus HTTP proxy.')
-parser.add_argument('-c', '--config', dest='config', default='config.yml',
-                    help='Path to configuration yaml-file. Default config.yml')
 parser.add_argument('--host', dest='host', default='0.0.0.0',
                     help='HTTP server host. Default 0.0.0.0')
 parser.add_argument('-p', '--port', dest='port', default=9126, type=int,
                     help='HTTP server port. Default 9126')
 args = parser.parse_args()
+
+HTTP_STATUS_ERROR = 500
 
 
 def create_app() -> web.Application:
@@ -21,43 +20,41 @@ def create_app() -> web.Application:
     return app
 
 
-def get_config() -> dict:
-    config_path = args.config
-    with open(config_path) as f:
-        config_data = yaml.load(f)
-    return config_data
-
-
-async def is_probe_success(target: dict, headers: dict) -> bool:
+async def _request(target: str, scheme: str, headers: dict, path: str, verify_ssl: bool) -> tuple:
     try:
-        url = target['url']
-        verify_ssl = target.get('verify_ssl', True)
+        url = scheme + '://' + target + path
+        verify_ssl = verify_ssl
 
         connector = TCPConnector(verify_ssl=verify_ssl)
         async with ClientSession(connector=connector) as session:
             with async_timeout.timeout(10):
                 async with session.get(url, headers=headers) as response:
-                    return await response.text()
+                    return (await response.text(), response.status, response.headers)
     except Exception as ex:
-        print(ex)
-        return False
+        return (str(ex), HTTP_STATUS_ERROR, {})
 
 async def index(request):
     return web.Response(text='<h1>HTTP proxy</h1><p><a href="/metrics">Metrics</a><p>', content_type='text/html')
 
 async def metrics(request):
-    result = ''
-    
-    auth_header = {}
-    if request.headers.get('Authorization'):
-        auth_header['Authorization'] = request.headers['Authorization']
-        
-    config = get_config()
-    for target in config.get('targets', []):
-        probe_success = await is_probe_success(target=target, headers=auth_header)
-        result += '%s\n' % (probe_success)
+    target = ''
+    path = '/metrics'
+    verify_ssl = 1
+    scheme = 'http'
 
-    return web.Response(text=result)
+    request_headers = request.headers
+    if request.query.get('target'):
+        target = request.query['target']
+    if request.query.get('path'):
+        path = request.query['path']
+    if request.query.get('verify_ssl'):
+        verify_ssl = request.query['verify_ssl']
+    if request.query.get('scheme'):
+        scheme = request.query['scheme']
+    result, status, response_headers = await _request(target=target, scheme=scheme, headers=request_headers,
+                                                      path=path, verify_ssl=bool(verify_ssl))
+
+    return web.Response(text=result, status=status, headers=response_headers)
 
 
 if __name__ == '__main__':
