231ffcb108e52884feca5eeff22fae64f4e9f779
sergey@telminov.ru <sergey@telminov.ru>
2017-08-18 17:46:02 +0300

    поправки

--- server.py
+++ server.py
@@ -25,29 +25,42 @@ async def _request(target: str, headers: dict, scheme: str = 'http', path: str =
     try:
         url = scheme + '://' + target + path
         verify_ssl = verify_ssl
+        print(f'url: {url}, verify_ssl: {verify_ssl}, headers: {headers}')
 
         connector = TCPConnector(verify_ssl=verify_ssl)
         async with ClientSession(connector=connector) as session:
             with async_timeout.timeout(10):
                 async with session.get(url, headers=headers) as response:
-                    return (await response.text(), response.status, response.headers)
+                    text = await response.text()
+                    print(f'response status {response.status}, response headers {response.headers}')
+                    return text, response.status, response.headers
     except Exception as ex:
-        return (str(ex), HTTP_STATUS_ERROR, {})
+        print(f'Exception: {ex}')
+        return str(ex), HTTP_STATUS_ERROR, {}
 
 async def index(request):
     return web.Response(text='<h1>HTTP proxy</h1><p><a href="/metrics">Metrics</a><p>', content_type='text/html')
 
 async def metrics(request):
     params = dict()
-    params['headers'] = request.headers
+
+    params['headers'] = {}
+    if request.headers.get('Authorization'):
+        params['headers'] = {'Authorization': request.headers['Authorization']}
+
     params['target'] = request.query['target']
 
     if request.query.get('scheme'):
         params['scheme'] = request.query.get('scheme')
+
     if request.query.get('path'):
         params['path'] = request.query.get('path')
+
     if request.query.get('verify_ssl'):
-        params['verify_ssl'] = request.query.get('verify_ssl')
+        verify_ssl = request.query.get('verify_ssl')
+        if str(verify_ssl).isdigit():
+            verify_ssl = int(verify_ssl)
+        params['verify_ssl'] = bool(verify_ssl)
 
     result, status, response_headers = await _request(**params)
 
